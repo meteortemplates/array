bfa55e84c60c55b96a2335234f1d9b06571acfcf
Andrew Reedy <andrew.reedy@thecontrolgroup.com>
2015-02-16 22:45:01 -0800

    Changed the logic to check for array change in the set function

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'templates:array',
   summary: 'Safe reactive arrays.',
-  version: '1.0.0',
+  version: '1.0.1',
   git: 'https://github.com/meteortemplates/array.git'
 });
 
